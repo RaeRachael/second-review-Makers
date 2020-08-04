5c9a41d08f559c8ada47a79558d6306314783a7d
Rachael <rachaelewins@hotmail.com>
2020-08-04 14:23:58 +0100

    renaming of Array and elements

--- lib/filter_pass.rb
+++ lib/filter_pass.rb
@@ -1,13 +1,13 @@
 
-def filter_pass(array)
+def filter_pass(frequencies)
   output = []
-  array.each do |freq|
-    if freq < 40
+  frequencies.each do |frequency|
+    if frequency < 40
       output << 40
-    elsif freq > 1000
+    elsif frequency > 1000
       output << 1000
     else
-      output << freq
+      output << frequency
     end
   end
   output
