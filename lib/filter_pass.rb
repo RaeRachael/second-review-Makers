5dd7a9609fd852275e8a0f330a2b6376133fab52
Rachael <rachaelewins@hotmail.com>
2020-08-04 14:16:53 +0100

    should make freq min value, 40, if less than 40

--- lib/filter_pass.rb
+++ lib/filter_pass.rb
@@ -1,4 +1,7 @@
 
 def filter_pass(array)
+  if array[0] < 40 
+    return [40]
+  end
   array
 end
--- spec/filter_pass_spec.rb
+++ spec/filter_pass_spec.rb
@@ -8,5 +8,8 @@ describe "#filter_pass" do
     it "should return '[58]' no change to freq" do
       expect(filter_pass([58])).to eq ([58])
     end
+    it "should make freq min value, 40, if less than 40" do
+      expect(filter_pass([1])).to eq ([40])
+    end
   end
 end
